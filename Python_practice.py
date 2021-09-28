0cc4c6c7694cc9b85e32eecb285bae95582119c8
JonathanBrown003 <jdbrown00003@gmail.com>
2021-09-27 23:56:47 -0500

    Adding the updated election files

---
+++ Python_practice.py
@@ -0,0 +1,6 @@
+counties_dict = {"arapahoe": 422829, "Denver": 463535, "Jefferson": 432438}
+for county in counties_dict:
+    print(county)
+
+for voters in counties_dict.values():
+    print(voters)    
